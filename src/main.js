abd426a894f7cc4689dae4c5f9ef931233b8af62
Keloran <keloran@develbox.info>
2023-01-04 15:59:14 +0000

    background-images are no longer inverted

--- src/main.js
+++ src/main.js
@@ -1,24 +1,11 @@
-function fixBackgroundImages() {
-  const parentAreas = document.getElementsByClassName("message-iframe-container");
-  if (parentAreas.length >= 1) {
-    const iFrame = parentAreas[0].firstChild.contentWindow.document;
-    const tags = iFrame.querySelectorAll("*");
-    tags.forEach(tag => {
-      let fixInvert = false;
 
-      if (getComputedStyle(tag).backgroundImage !== 'none') {
-        fixInvert = true;
-      }
+import { ExtensionRegistry } from 'mailspring-exports';
 
-      // using this method so more can be added later
-      if (fixInvert) {
-        tag.style.filter = "invert(1)";
-      }
-    })
-  }
-}
-
+import BackgroundImageFix from './background-image-fix';
 
 export function activate() {
-  fixBackgroundImages();
+  ExtensionRegistry.MessageView.register(BackgroundImageFix)
+}
+export function deactivate() {
+  ExtensionRegistry.MessageView.unregister(BackgroundImageFix)
 }
